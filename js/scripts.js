70f7dcf85a3224578f00ea5a1f542916b2a640cf
Matthew Craig <mgcraig78@gmail.com>
2019-10-05 09:39:15 -0700

    Make JS function have a 'try again' result

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 var pingPong = function(entry) {
-    if (entry % 3 === 0) {
-      return "ping";
+    if (entry % 15 === 0) {
+      return "pingpong";
     }
     else if (entry % 5 === 0) {
       return "pong";
     }
-    else if (entry % 15 === 0) {
-      return "pingpong";
+    else if (entry % 3 === 0) {
+      return "ping";
+    }
+    else if (entry % 15 !== 0 || entry % 5 !== 0 || entry % 3 !== 0) {
+        return "try again";
     };
 };
+
     
   $(document).ready(function() {
     $("form#ping-pong").submit(function(event) {
